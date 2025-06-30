379b7d3298dff940a7e126a4d3da0bb081a4bb71
ShlyaptsevaMarina <testmary103@outlook.com>
2025-06-30 13:04:17 +0300

    Удалены устаревшие SQL запросы

--- SQL/SQL1.sql
+++
@@ -1,5 +0,0 @@
-SELECT public.projects.project_name, titles.titel_name, AVG(positions.salary)
-FROM public.projects JOIN positions ON public.projects.project_id = positions.project_id
-JOIN titles ON titles.ID = positions.title_id
-WHERE titles.titel_name = 'tester' AND public.projects.project_name = 'ПУМЧД'
-GROUP BY public.projects.project_name, titles.titel_name;
--- SQL/SQL2.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-  CONCAT(employee.first_name, ' ', employee.last_name),
-  titles.titel_name
-FROM positions
-JOIN employee ON positions.employee_id = employee.employee_id
-JOIN titles ON positions.title_id = titles.id
-GROUP BY employee.employee_id, titles.titel_name, employee.first_name, employee.last_name
-HAVING COUNT(DISTINCT positions.project_id) > 1;
